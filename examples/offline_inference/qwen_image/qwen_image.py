61ae3a638140cfa6820cba1d5ec04ac40a8b2071
zjy0516 <riverclouds.zhu@qq.com>
2025-11-25 23:02:13 +0800

    update example
    Signed-off-by: zjy0516 <riverclouds.zhu@qq.com>

--- examples/offline_inference/qwen_image/qwen_image.py
+++ examples/offline_inference/qwen_image/qwen_image.py
@@ -1,18 +1,11 @@
 from vllm_omni.diffusion.omni_diffusion import OmniDiffusion
 
-# m = OmniDiffusion.from_pretrained(
-#     engine_args={
-#         "model_name_or_path": "test-model",
-#         "num_gpus": 1,
-#     }
-# )
-# m.generate("A beautiful painting of a sunset over the mountains.")
 model_name = "Qwen/Qwen-Image"
 prompt = "a cup of coffee on the table"
 if __name__ == "__main__":
     m = OmniDiffusion.from_pretrained(
         model="Qwen/Qwen-Image",
-        num_gpus=1,  # Adjust based on your hardware
+        num_gpus=1,
     )
     image = m.generate(prompt)
     image[0].save("qwen_image_output.png")
