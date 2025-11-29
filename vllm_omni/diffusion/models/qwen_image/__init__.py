b681c4159234dbf15f0fa4d7997a76d25b041822
samithuang <285365963@qq.com>
2025-11-29 16:37:32 +0000

    add init
    Signed-off-by: samithuang <285365963@qq.com>

--- vllm_omni/diffusion/models/qwen_image/__init__.py
+++ vllm_omni/diffusion/models/qwen_image/__init__.py
@@ -2,3 +2,16 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 """Qwen Image model components."""
 
+from vllm_omni.diffusion.models.qwen_image.pipeline_qwen_image import (
+    QwenImagePipeline,
+    get_qwen_image_post_process_func,
+)
+from vllm_omni.diffusion.models.qwen_image.qwen_image_transformer import (
+    QwenImageTransformer2DModel,
+)
+
+__all__ = [
+    "QwenImagePipeline",
+    "QwenImageTransformer2DModel",
+    "get_qwen_image_post_process_func",
+]
