ca509e6b890ee42fdfc17f797a38dc0ed8b9d993
zjy0516 <riverclouds.zhu@qq.com>
2025-12-03 13:30:04 +0800

    ci
    Signed-off-by: zjy0516 <riverclouds.zhu@qq.com>

--- .buildkite/scripts/setup_env.sh
+++ .buildkite/scripts/setup_env.sh
@@ -49,4 +49,3 @@ VENV_PYTHON="${VENV_DIR}/bin/python"
 
 "${UV_BIN}" pip install --python "${VENV_PYTHON}" vllm==0.11.0
 "${UV_BIN}" pip install --python "${VENV_PYTHON}" -e ".[dev]"
-"${VENV_PYTHON}" -m pytest -v -s tests/test_omni_llm.py
