94f6fc9d443d9df20f1d23386bf5eb0e33e11b0c
zjy0516 <riverclouds.zhu@qq.com>
2025-12-03 16:59:34 +0800

    update test
    Signed-off-by: zjy0516 <riverclouds.zhu@qq.com>

--- tests/test_diffusion_model.py
+++ tests/test_diffusion_model.py
@@ -12,15 +12,17 @@ def test_diffusion_model(model_name: str):
     # high resolution may cause OOM on L4
     height = 256
     width = 256
-    image = m.generate(
+    images = m.generate(
         "a photo of a cat sitting on a laptop keyboard",
         height=height,
         width=width,
-        num_inference_steps=9,
+        num_inference_steps=2,
         guidance_scale=0.0,
         generator=torch.Generator("cuda").manual_seed(42),
+        num_outputs_per_prompt=2,
     )
+    assert len(images) == 2
     # check image size
-    assert image[0].width == width
-    assert image[0].height == height
-    image[0].save("z_image_output.png")
+    assert images[0].width == width
+    assert images[0].height == height
+    images[0].save("z_image_output.png")
