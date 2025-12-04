6b367fbe65658ca96b8080bc819c3e297f51016c
Claisenn <1726677248@qq.com>
2025-12-04 11:38:52 +0800

    [Bugfix] Init early toch cuda (#185)
    Signed-off-by: n00909098 <nguyen.kha.long@huawei.com>
    Co-authored-by: n00909098 <nguyen.kha.long@huawei.com>

--- vllm_omni/diffusion/utils/hf_utils.py
+++ vllm_omni/diffusion/utils/hf_utils.py
@@ -1,12 +1,13 @@
 from functools import lru_cache
 
-from diffusers.pipelines.pipeline_utils import DiffusionPipeline
 from vllm.logger import init_logger
 
 logger = init_logger(__name__)
 
 
 def load_diffusers_config(model_name) -> dict:
+    from diffusers.pipelines.pipeline_utils import DiffusionPipeline
+
     config = DiffusionPipeline.load_config(model_name)
     return config
 
