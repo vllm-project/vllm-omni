82d752ba9ab019d9a8896425b0f754496bb99ea5
linyueqian <linyueqian@outlook.com>
2025-12-10 16:38:05 -1000

    [Model] Add Wan2.2 text-to-video support (#202)
    Signed-off-by: linyueqian <linyueqian@outlook.com>

--- vllm_omni/diffusion/registry.py
+++ vllm_omni/diffusion/registry.py
@@ -29,6 +29,11 @@ _DIFFUSION_MODELS = {
         "pipeline_flux2",
         "Flux2Pipeline",
     ),
+    "WanPipeline": (
+        "wan2_2",
+        "pipeline_wan2_2",
+        "Wan22Pipeline",
+    ),
 }
 
 
@@ -67,6 +72,7 @@ _DIFFUSION_POST_PROCESS_FUNCS = {
     "QwenImageEditPipeline": "get_qwen_image_edit_post_process_func",
     "ZImagePipeline": "get_post_process_func",
     "Flux2Pipeline": "get_flux2_post_process_func",
+    "WanPipeline": "get_wan22_post_process_func",
 }
 
 _DIFFUSION_PRE_PROCESS_FUNCS = {
