eb7804ea826c7db5fdf06465f234d370d4e2df61
Didan Deng <33117903+wtomin@users.noreply.github.com>
2025-12-12 17:55:32 +0800

    smaller head size
    Signed-off-by: Didan Deng <33117903+wtomin@users.noreply.github.com>

--- tests/diffusion/attention/test_ulysses_sequence_parallel.py
+++ tests/diffusion/attention/test_ulysses_sequence_parallel.py
@@ -135,7 +135,7 @@ class TestMultiLayerAttentionModel(torch.nn.Module):
 @pytest.mark.parametrize("batch_size", [2])
 @pytest.mark.parametrize("seq_len", [16])
 @pytest.mark.parametrize("num_heads", [8])
-@pytest.mark.parametrize("head_size", [32])
+@pytest.mark.parametrize("head_size", [8])
 @pytest.mark.parametrize("causal", [True])
 @pytest.mark.parametrize("dtype", [torch.float16, torch.bfloat16])
 @pytest.mark.parametrize("use_sync", [True, False])
