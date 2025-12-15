465e90e5437d788a958d81937924828104c93cdc
gcanlin <canlinguosdu@gmail.com>
2025-12-15 03:51:18 +0000

    add type_name
    Signed-off-by: gcanlin <canlinguosdu@gmail.com>

--- vllm_omni/distributed/omni_connectors/utils/serialization.py
+++ vllm_omni/distributed/omni_connectors/utils/serialization.py
@@ -4,7 +4,15 @@
 import struct
 from typing import Any
 
+import numpy as np
+import torch
+
 from vllm.v1.serial_utils import MsgpackDecoder, MsgpackEncoder
+
+# Type codes for serialization
+_TYPE_GENERIC = 0
+_TYPE_TENSOR = 1
+_TYPE_NDARRAY = 2
 
 
 class OmniSerializer:
@@ -14,19 +22,29 @@ class OmniSerializer:
     Wraps vLLM's MsgpackEncoder/MsgpackDecoder for safe serialization.
 
     Wire format:
-        [num_bufs: u32][len_0: u32][len_1: u32]...[buf_0][buf_1]...
+        [type: u8][num_bufs: u32][len_0: u32][len_1: u32]...[buf_0][buf_1]...
     """
 
     _encoder = MsgpackEncoder()
     _decoder = MsgpackDecoder()
+    _tensor_decoder = MsgpackDecoder(torch.Tensor)
+    _ndarray_decoder = MsgpackDecoder(np.ndarray)
 
     @staticmethod
     def serialize(obj: Any) -> bytes:
         """Serialize an object to bytes."""
+        # Determine type code
+        if isinstance(obj, torch.Tensor):
+            type_code = _TYPE_TENSOR
+        elif isinstance(obj, np.ndarray):
+            type_code = _TYPE_NDARRAY
+        else:
+            type_code = _TYPE_GENERIC
+
         bufs = OmniSerializer._encoder.encode(obj)
 
-        # Header: num_bufs + length of each buffer
-        header = struct.pack("<I", len(bufs))
+        # Header: type_code + num_bufs + length of each buffer
+        header = struct.pack("<BI", type_code, len(bufs))
         for buf in bufs:
             header += struct.pack("<I", len(buf))
 
@@ -40,12 +58,20 @@ class OmniSerializer:
     def deserialize(data: bytes) -> Any:
         """Deserialize bytes to an object."""
         # Parse header
-        (num_bufs,) = struct.unpack("<I", data[:4])
-        header_size = 4 + num_bufs * 4
-        len_arr = struct.unpack(f"<{num_bufs}I", data[4:header_size])
+        type_code, num_bufs = struct.unpack("<BI", data[:5])
+        header_size = 5 + num_bufs * 4
+        len_arr = struct.unpack(f"<{num_bufs}I", data[5:header_size])
+
+        # Select decoder based on type
+        if type_code == _TYPE_TENSOR:
+            decoder = OmniSerializer._tensor_decoder
+        elif type_code == _TYPE_NDARRAY:
+            decoder = OmniSerializer._ndarray_decoder
+        else:
+            decoder = OmniSerializer._decoder
 
         if num_bufs == 1:
-            return OmniSerializer._decoder.decode(data[header_size:])
+            return decoder.decode(data[header_size:])
 
         # Multi-buffer: split by lengths
         bufs = []
@@ -53,4 +79,4 @@ class OmniSerializer:
         for length in len_arr:
             bufs.append(data[offset : offset + length])
             offset += length
-        return OmniSerializer._decoder.decode(bufs)
+        return decoder.decode(bufs)
