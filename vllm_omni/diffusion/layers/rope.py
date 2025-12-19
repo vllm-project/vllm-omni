24c33947a807a7de0140e7e5e06e3a5e0c90e069
zjy0516 <riverclouds.zhu@qq.com>
2025-12-19 10:25:55 +0800

    update
    Signed-off-by: zjy0516 <riverclouds.zhu@qq.com>

--- vllm_omni/diffusion/layers/rope.py
+++ vllm_omni/diffusion/layers/rope.py
@@ -54,6 +54,11 @@ class RotaryEmbedding(CustomOp):
     ) -> torch.Tensor:
         from vllm.vllm_flash_attn.layers.rotary import apply_rotary_emb
 
+        if cos.dim() == 3:
+            # (B, S, D/2) -> (S, D/2)
+            cos = cos[0]
+            sin = sin[0]
+
         return apply_rotary_emb(
             x,
             cos,
