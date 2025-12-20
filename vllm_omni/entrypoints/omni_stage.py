dfaebd6e3f1a98553252b25d4fe42c02923b3b07
Taichang Zhou <tzhouam@connect.ust.hk>
2025-12-21 02:30:33 +0800

    follow pre-commit
    Signed-off-by: Taichang Zhou <tzhouam@connect.ust.hk>

--- vllm_omni/entrypoints/omni_stage.py
+++ vllm_omni/entrypoints/omni_stage.py
@@ -627,7 +627,11 @@ def _stage_worker(
             try:
                 _os.close(lock_fd)
                 _logging.getLogger(__name__).debug("[Stage-%s] Released initialization lock (fd=%s)", stage_id, lock_fd)
-                _logging.getLogger(__name__).info("[Stage-%s] Stage successfully initialized with %s seconds including waiting for locks", stage_id, _time.time() - wait_start)
+                _logging.getLogger(__name__).info(
+                    "[Stage-%s] Stage successfully initialized with %s seconds including waiting for locks",
+                    stage_id,
+                    _time.time() - wait_start,
+                )
             except (OSError, ValueError):
                 pass
 
@@ -1112,7 +1116,11 @@ async def _stage_worker_async(
             try:
                 _os.close(lock_fd)
                 _logging.getLogger(__name__).debug("[Stage-%s] Released initialization lock (fd=%s)", stage_id, lock_fd)
-                _logging.getLogger(__name__).info("[Stage-%s] Stage successfully initialized with %s seconds including waiting for locks", stage_id, _time.time() - wait_start)
+                _logging.getLogger(__name__).info(
+                    "[Stage-%s] Stage successfully initialized with %s seconds including waiting for locks",
+                    stage_id,
+                    _time.time() - wait_start,
+                )
             except (OSError, ValueError):
                 pass
     omni_stage.set_async_engine(stage_engine)
