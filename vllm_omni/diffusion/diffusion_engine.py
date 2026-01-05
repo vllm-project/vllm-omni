cf6fae7668decb233ff3a975a4ba931dfb645c14
Ziming Huang <hzm414167@alibaba-inc.com>
2026-01-05 14:53:10 +0800

    [BugFix] AttributeError: 'AsyncOmniDiffusion' object has no attribute 'abort' (#624)
    Signed-off-by: ZeldaHuang <hzm414167@alibaba-inc.com>
    Signed-off-by: DefTruth <qiustudent_r@163.com>

--- vllm_omni/diffusion/diffusion_engine.py
+++ vllm_omni/diffusion/diffusion_engine.py
@@ -4,7 +4,7 @@
 import multiprocessing as mp
 import time
 import weakref
-from collections.abc import Callable
+from collections.abc import Callable, Iterable
 from dataclasses import dataclass
 from typing import Any
 
@@ -361,3 +361,8 @@ class DiffusionEngine:
 
     def close(self) -> None:
         self._finalizer()
+
+    def abort(self, request_id: str | Iterable[str]) -> None:
+        # TODO implement it
+        logger.warning("DiffusionEngine abort is not implemented yet")
+        pass
