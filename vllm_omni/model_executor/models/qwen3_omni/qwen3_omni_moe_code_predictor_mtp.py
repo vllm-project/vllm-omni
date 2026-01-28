8be762f4b92d1836119684b7ec7d09e67ae0652d
ZeldaHuang <hzm414167@alibaba-inc.com>
2026-01-29 01:34:25 +0800

    fix
    Signed-off-by: ZeldaHuang <hzm414167@alibaba-inc.com>

--- vllm_omni/model_executor/models/qwen3_omni/qwen3_omni_moe_code_predictor_mtp.py
+++ vllm_omni/model_executor/models/qwen3_omni/qwen3_omni_moe_code_predictor_mtp.py
@@ -460,7 +460,7 @@ class Qwen3OmniMoeTalkerCodePredictor(nn.Module):
             ]
         )
 
-        max_batch_size = vllm_config.scheduler_config.max_num_batched_tokens
+        max_batch_size = vllm_config.scheduler_config.max_num_seqs
         self.register_buffer(
             "position_ids_buffer",
             torch.arange(self.num_code_groups, dtype=torch.int64, device=next(self.parameters()).device)
