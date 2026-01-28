de2d3e5d729c5d84de96398b350e99e780692a53
wangyu31577 <wangyu31577@hundsun.com>
2026-01-28 23:23:21 +0800

    Optimize the log printing
    Signed-off-by: wangyu31577 <wangyu31577@hundsun.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,7 @@ os.environ["VLLM_WORKER_MULTIPROC_METHOD"] = "spawn"
 if "VLLM_TARGET_DEVICE" not in os.environ:
     os.environ["VLLM_TARGET_DEVICE"] = "cpu"
 
+import gc
 import socket
 import subprocess
 import sys
@@ -63,6 +64,11 @@ def _run_pre_test_cleanup(enable_force=False):
     if os.getenv("VLLM_TEST_CLEAN_GPU_MEMORY", "0") != "1" and not enable_force:
         print("GPU cleanup disabled")
         return
+
+    if torch.cuda.is_available():
+        gc.collect()
+        torch.cuda.empty_cache()
+
     print("Pre-test GPU status:")
 
     num_gpus = torch.cuda.device_count()
@@ -82,8 +88,6 @@ def _run_post_test_cleanup(enable_force=False):
     if os.getenv("VLLM_TEST_CLEAN_GPU_MEMORY", "0") != "1" and not enable_force:
         print("GPU cleanup disabled")
         return
-
-    import gc
 
     if torch.cuda.is_available():
         gc.collect()
