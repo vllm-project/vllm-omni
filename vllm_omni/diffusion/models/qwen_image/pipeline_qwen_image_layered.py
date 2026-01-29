da2b3072c77d06540df6077157d331337485638f
Didan Deng <33117903+wtomin@users.noreply.github.com>
2026-01-29 17:29:27 +0800

    fix error
    Signed-off-by: Didan Deng <33117903+wtomin@users.noreply.github.com>

--- vllm_omni/diffusion/models/qwen_image/pipeline_qwen_image_layered.py
+++ vllm_omni/diffusion/models/qwen_image/pipeline_qwen_image_layered.py
@@ -189,6 +189,7 @@ def retrieve_latents(
 
 class QwenImageLayeredPipeline(nn.Module, SupportImageInput, QwenImageCFGParallelMixin):
     color_format = "RGBA"
+
     def __init__(
         self,
         *,
