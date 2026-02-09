85517443df20abb80c0ba51a4305d68ad2b5b9a1
yenuo26 <410167048@qq.com>
2026-02-09 19:07:56 +0800

    fix conflicts
    Signed-off-by: yenuo26 <410167048@qq.com>

--- tests/e2e/offline_inference/test_qwen2_5_omni.py
+++ tests/e2e/offline_inference/test_qwen2_5_omni.py
@@ -12,11 +12,10 @@ from vllm.assets.image import ImageAsset
 from vllm.assets.video import VideoAsset
 from vllm.envs import VLLM_USE_MODELSCOPE
 from vllm.multimodal.image import convert_image_mode
-
-from tests.utils import create_new_process_for_each_test, hardware_test
 from vllm_omni.platforms import current_omni_platform
 
-from .conftest import OmniRunner
+from tests.conftest import OmniRunner
+from .utils import create_new_process_for_each_test, hardware_test
 
 models = ["Qwen/Qwen2.5-Omni-3B"]
 
