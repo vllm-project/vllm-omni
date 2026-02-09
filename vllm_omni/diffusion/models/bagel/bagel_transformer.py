d59273c7bd130cffa2d084d701b045a1c1044f11
Ding Zuhao <e1583181@u.nus.edu>
2026-02-10 00:08:05 +0800

    fix: add missing tqdm import
    Signed-off-by: Ding Zuhao <e1583181@u.nus.edu>

--- vllm_omni/diffusion/models/bagel/bagel_transformer.py
+++ vllm_omni/diffusion/models/bagel/bagel_transformer.py
@@ -14,6 +14,7 @@ from typing import Any, Dict, Optional, Tuple
 
 import numpy as np
 import torch
+from tqdm import tqdm
 from torch import nn
 from torch.nn.attention.flex_attention import flex_attention
 from transformers.models.qwen2.configuration_qwen2 import Qwen2Config
