0124341bc7cd8e3d8c57ab07c011ecdcfd309f40
yenuo26 <410167048@qq.com>
2026-02-11 16:50:04 +0800

    Update process cleanup logic in OmniRunner
    This commit modifies the `_cleanup_process` method in the `OmniRunner` class to remove the "vllm" keyword from the process termination logic, focusing solely on processes related to "core". This change streamlines the cleanup process during context manager exit.
    
    Signed-off-by: yenuo26 <410167048@qq.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1572,7 +1572,7 @@ class OmniRunner:
 
     def _cleanup_process(self):
         try:
-            keywords = ["vllm", "core"]
+            keywords = ["core"]
 
             for proc in psutil.process_iter(["pid", "name", "cmdline", "username"]):
                 try:
