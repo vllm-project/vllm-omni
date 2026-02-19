279f7caa9a7371d25478b25f5af73a9d6cf3519e
Isotr0py <mozf@mail2.sysu.edu.cn>
2026-02-19 12:55:05 +0800

    pre-commit
    Signed-off-by: Isotr0py <mozf@mail2.sysu.edu.cn>

--- vllm_omni/diffusion/model_loader/diffusers_loader.py
+++ vllm_omni/diffusion/model_loader/diffusers_loader.py
@@ -274,7 +274,4 @@ class DiffusersPipelineLoader:
         if loaded_weights is not None:
             weights_not_loaded = weights_to_load - loaded_weights
             if weights_not_loaded:
-                raise ValueError(
-                    "Following weights were not initialized from "
-                    f"checkpoint: {weights_not_loaded}"
-                )
+                raise ValueError(f"Following weights were not initialized from checkpoint: {weights_not_loaded}")
