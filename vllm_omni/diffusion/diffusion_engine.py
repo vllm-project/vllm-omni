76b17390c86ac55e49f48d4ce4df0dcd056a4868
Chen Yang <2082464740@qq.com>
2026-02-25 16:41:36 +0800

    Compatible with vllm-omni 0.16.0rc
    Signed-off-by: Chen Yang <2082464740@qq.com>

--- vllm_omni/diffusion/diffusion_engine.py
+++ vllm_omni/diffusion/diffusion_engine.py
@@ -118,9 +118,14 @@ class DiffusionEngine:
             "postprocess_time_ms": round(postprocess_time * 1000,2),
             "image_num": int(request.sampling_params.num_outputs_per_prompt),
             "resolution": int(request.sampling_params.resolution),
+            "postprocess_time_ms": postprocess_time * 1000,
             "denoise_time_per_step_ms": 0.0,
             "vae_time_ms": 0.0,
         }
+        if self.pre_process_func is not None:
+            metrics["preprocessing_time_ms"] = preprocess_time * 1000
+
+         # Handle single request or multiple requests
 
 
         dit_time_seconds = metrics["dit_time_ms"] / 1000
