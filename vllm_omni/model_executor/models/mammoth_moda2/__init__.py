b92d12f65f8c639ddbeb7268cb27842311a91cea
HonestDeng <2958906959@qq.com>
2026-03-03 16:33:20 +0800

    mv tokenizer to a new folder
    Signed-off-by: HonestDeng <2958906959@qq.com>

--- vllm_omni/model_executor/models/mammoth_moda2/__init__.py
+++ vllm_omni/model_executor/models/mammoth_moda2/__init__.py
@@ -1,12 +1,12 @@
 from transformers import AutoTokenizer
 
+from vllm_omni.tokenizers.mammoth_moda2_tokenizer import MammothUTokenizer
 from vllm_omni.transformers_utils.configs.mammoth_moda2 import (  # noqa: F401 registers AutoConfig
     Mammothmoda2Config,
     Mammothmoda2Qwen2_5_VLConfig,
 )
 
 from .mammoth_moda2 import MammothModa2ARForConditionalGeneration
-from .tokenizer import MammothUTokenizer
 
 # AutoConfig.register is done inside transformers_utils/configs/mammoth_moda2.py
 AutoTokenizer.register(config_class=Mammothmoda2Config, slow_tokenizer_class=MammothUTokenizer)
