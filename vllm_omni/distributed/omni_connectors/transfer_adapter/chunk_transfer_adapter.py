0213d6d723dbd94ea6f818007aad265b7d2e3f52
JuanPZuluaga <juanz9312@gmail.com>
2026-03-16 18:33:37 +0000

    add cleanup cached_ic
    Signed-off-by: JuanPZuluaga <juanz9312@gmail.com>

--- vllm_omni/distributed/omni_connectors/transfer_adapter/chunk_transfer_adapter.py
+++ vllm_omni/distributed/omni_connectors/transfer_adapter/chunk_transfer_adapter.py
@@ -276,6 +276,10 @@ class OmniChunkTransferAdapter(OmniTransferAdapterBase):
         self.request_payload.pop(external_req_id, None)
         self.code_prompt_token_ids.pop(external_req_id, None)
 
+        cached_ic = getattr(self, "_cached_ic", None)
+        if cached_ic is not None:
+            cached_ic.pop(external_req_id, None)
+
     ########################################################################
     # Schedule Helper
     ########################################################################
