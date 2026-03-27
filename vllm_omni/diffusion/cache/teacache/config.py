3bad32e6a8aedf257f8022eb38d69f3302c3a0a8
wuzhongjian <wuzhongjian_yewu@cmss.chinamobile.com>
2026-03-27 14:22:16 +0800

    [Feature]: support Flux.2-dev tea_cache Signed-off-by: wuzhongjian <wuzhongjian_yewu@cmss.chinamobile.com>

--- vllm_omni/diffusion/cache/teacache/config.py
+++ vllm_omni/diffusion/cache/teacache/config.py
@@ -54,6 +54,15 @@ _MODEL_COEFFICIENTS = {
         -12.281286412689623,
         1.0733905006198015,
     ],
+    # Flux2 transformer coefficients
+    # Copied from Qwen-Image, need to be tuned specifically for Flux2 in future
+    "Flux2Transformer2DModel": [
+        -4.50000000e02,
+        2.80000000e02,
+        -4.50000000e01,
+        3.20000000e00,
+        -2.00000000e-02,
+    ],
 }
 
 
