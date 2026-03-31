10fe27037a42c979ae6d3b47f74e51614452e37c
lishilin314 <lishilin314@163.com>
2026-03-31 07:31:10 +0000

    optimize NPU layernorm algorithm
    Co-authored-by: vasede <1399968934@qq.com>
    Signed-off-by: Li-8916 <lishilin314@163.com>

--- vllm_omni/diffusion/layers/rope.py
+++ vllm_omni/diffusion/layers/rope.py
@@ -49,9 +49,6 @@ def apply_rotary_emb_mindiesd(
         # (B, S, D/2) -> (S, D/2)
         cos = cos[0]
         sin = sin[0]
-    if cos.shape[-1] == x.shape[-1]:
-        half_head_dim = False
-        # already expanded to (S, D), just use directly
     if interleaved:
         # if last dim of sin and cos is D/2, expand to (S, D) to adapt to mindiesd operators
         if half_head_dim:
