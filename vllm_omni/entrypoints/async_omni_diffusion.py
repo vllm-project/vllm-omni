78e9f49a4462a49580395846311927012a9f33f6
erfgss <97771661+erfgss@users.noreply.github.com>
2026-04-03 09:30:43 +0800

    Refactor model configuration assignment logic
    Signed-off-by: erfgss <97771661+erfgss@users.noreply.github.com>

--- vllm_omni/entrypoints/async_omni_diffusion.py
+++ vllm_omni/entrypoints/async_omni_diffusion.py
@@ -110,10 +110,7 @@ class AsyncOmniDiffusion:
             if config_dict is not None:
                 if od_config.model_class_name is None:
                     od_config.model_class_name = config_dict.get("_class_name", None)
-
-                if od_config.model_class_name == "DreamIDOmniPipeline":
-                    model_config = {k: v for k, v in config_dict.items() if k != "_class_name"}
-                    od_config.model_config = model_config
+                    od_config.model_config = {k: v for k, v in config_dict.items() if k != "_class_name"}
                 od_config.update_multimodal_support()
 
                 tf_config_dict = get_hf_file_to_dict("transformer/config.json", od_config.model)
