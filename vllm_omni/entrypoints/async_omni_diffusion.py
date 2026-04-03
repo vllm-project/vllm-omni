d1fcc5422595fd3f642d1069b00f9640dd1f9287
erfgss <97771661+erfgss@users.noreply.github.com>
2026-04-03 15:11:38 +0800

    Update async_omni_diffusion.py
    Signed-off-by: erfgss <97771661+erfgss@users.noreply.github.com>

--- vllm_omni/entrypoints/async_omni_diffusion.py
+++ vllm_omni/entrypoints/async_omni_diffusion.py
@@ -110,7 +110,6 @@ class AsyncOmniDiffusion:
             if config_dict is not None:
                 if od_config.model_class_name is None:
                     od_config.model_class_name = config_dict.get("_class_name", None)
-                    od_config.model_config = {k: v for k, v in config_dict.items() if k != "_class_name"}
                 od_config.update_multimodal_support()
 
                 tf_config_dict = get_hf_file_to_dict("transformer/config.json", od_config.model)
