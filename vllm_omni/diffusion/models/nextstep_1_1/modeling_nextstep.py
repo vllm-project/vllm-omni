46940682b29b1cde59ca1eab89b9ba6b7aa7f8b0
Parth Bansal <parthbansal127@gmail.com>
2026-04-04 12:13:38 +0000

    update
    Signed-off-by: Parth Bansal <parthbansal127@gmail.com>

--- vllm_omni/diffusion/models/nextstep_1_1/modeling_nextstep.py
+++ vllm_omni/diffusion/models/nextstep_1_1/modeling_nextstep.py
@@ -122,8 +122,10 @@ class NextStepModel(nn.Module):
 
         self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx)
         self.layers = nn.ModuleList(
-            [LlamaDecoderLayer(config, layer_idx, quant_config=quant_config)
-             for layer_idx in range(config.num_hidden_layers)]
+            [
+                LlamaDecoderLayer(config, layer_idx, quant_config=quant_config)
+                for layer_idx in range(config.num_hidden_layers)
+            ]
         )
         self.norm = LlamaRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
         self.rotary_emb = LlamaRotaryEmbedding(config=config)
