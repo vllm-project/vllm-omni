999e849e7a3561d01bce27b99e07a3e0ec0c4388
wangyu <410167048@qq.com>
2026-04-21 09:35:53 +0800

    Remove redundant pytest.mark.core_model decorators from test_qwen3_tts_base_expansion.py to streamline test definitions.
    Signed-off-by: wangyu <410167048@qq.com>

--- tests/e2e/online_serving/test_qwen3_tts_base_expansion.py
+++ tests/e2e/online_serving/test_qwen3_tts_base_expansion.py
@@ -63,7 +63,6 @@ tts_server_params = [
 ]
 
 
-@pytest.mark.core_model
 @hardware_test(res={"cuda": "L4"}, num_cards=1)
 @pytest.mark.parametrize("omni_server", tts_server_params, indirect=True)
 def test_voice_clone_streaming_001(omni_server, openai_client) -> None:
@@ -89,7 +88,6 @@ def test_voice_clone_streaming_001(omni_server, openai_client) -> None:
     openai_client.send_audio_speech_request(request_config, request_num=get_max_batch_size("few"))
 
 
-@pytest.mark.core_model
 @hardware_test(res={"cuda": "L4"}, num_cards=1)
 @pytest.mark.parametrize("omni_server", tts_server_params, indirect=True)
 def test_response_format_001(omni_server, openai_client) -> None:
