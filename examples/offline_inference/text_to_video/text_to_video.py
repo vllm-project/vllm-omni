c6b666eeb14cb8e140cf225359474089fc3ae094
xiaohajiayou <923390377@qq.com>
2026-04-24 12:33:16 +0800

    Add nullify handling to offline example parsers
    Signed-off-by: xiaohajiayou <923390377@qq.com>

--- examples/offline_inference/text_to_video/text_to_video.py
+++ examples/offline_inference/text_to_video/text_to_video.py
@@ -226,6 +226,9 @@ def parse_args() -> argparse.Namespace:
         default=1,
         help="Number of HSDP replica groups.",
     )
+    from vllm_omni.engine.arg_utils import nullify_stage_engine_defaults
+
+    nullify_stage_engine_defaults(parser)
     return parser.parse_args()
 
 
