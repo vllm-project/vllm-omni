1186c52a3324b592504211c9aac9026ef1862ff4
Chenguang ZHENG <645327136@qq.com>
2026-05-20 15:15:30 +0800

    Fix Hunyuan worker device context
    Signed-off-by: Chenguang ZHENG <645327136@qq.com>

--- vllm_omni/diffusion/distributed/utils.py
+++ vllm_omni/diffusion/distributed/utils.py
@@ -10,5 +10,7 @@ from vllm_omni.platforms import current_omni_platform
 
 def get_local_device() -> torch.device:
     """Return the torch device for the current rank based on detected device type."""
+    if torch.cuda.is_available() and torch.cuda.is_initialized():
+        return current_omni_platform.get_torch_device(torch.accelerator.current_device_index())
     local_rank = int(os.environ.get("LOCAL_RANK", 0))
     return current_omni_platform.get_torch_device(local_rank)
