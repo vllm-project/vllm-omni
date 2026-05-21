b9cee9aff4d9611268dc11b0348300a8db365c5b
ksiyuan <ksiyuan@umich.edu>
2026-05-21 14:37:29 +0800

    fix
    Signed-off-by: ksiyuan <ksiyuan@umich.edu>

---
+++ tests/model_executor/models/qwen2_5_omni/test_weight_loading.py
@@ -0,0 +1,30 @@
+import pytest
+from vllm_ascend.quantization.modelslim_config import get_linear_quant_type
+
+
+class TestQwen2_5OmniWeightLoading:
+    def test_qkv_weight_mapping(self):
+        mock_quant_description = {
+            "visual.blocks.0.attn.q.weight": {"quant_type": "FLOAT"},
+            "visual.blocks.0.attn.k.weight": {"quant_type": "FLOAT"},
+            "visual.blocks.0.attn.v.weight": {"quant_type": "FLOAT"},
+        }
+
+        test_prefix = "visual.blocks.0.attn.qkv"
+        packed_modules_mapping = {
+            "attn_akv_proj": [
+                "attn_q_proj",
+                "attn_k_proj",
+                "attn_v_proj",
+            ],
+            "qkv": [
+                "q",
+                "k",
+                "v",
+            ],
+        }
+
+        try:
+            _ = get_linear_quant_type(mock_quant_description, test_prefix, packed_modules_mapping)
+        except KeyError as e:
+            pytest.fail(f"KeyError was raised: {e}\n")
