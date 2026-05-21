b2386cd0ec304b4b08a530a1c00b7652fe0726db
ksiyuan <ksiyuan@umich.edu>
2026-05-21 15:43:47 +0800

    fix
    Signed-off-by: ksiyuan <ksiyuan@umich.edu>

--- tests/model_executor/models/qwen2_5_omni/test_weight_loading.py
+++ tests/model_executor/models/qwen2_5_omni/test_weight_loading.py
@@ -1,5 +1,4 @@
 import pytest
-from vllm_ascend.quantization.modelslim_config import get_linear_quant_type
 
 pytestmark = [pytest.mark.core_model, pytest.mark.cpu]
 
@@ -12,7 +11,7 @@ class TestQwen2_5OmniWeightLoading:
             "visual.blocks.0.attn.v.weight": {"quant_type": "FLOAT"},
         }
 
-        test_prefix = "visual.blocks.0.attn.qkv"
+        prefix = "visual.blocks.0.attn.qkv"
         packed_modules_mapping = {
             "attn_akv_proj": [
                 "attn_q_proj",
@@ -27,6 +26,24 @@ class TestQwen2_5OmniWeightLoading:
         }
 
         try:
-            _ = get_linear_quant_type(mock_quant_description, test_prefix, packed_modules_mapping)
+            proj_name = prefix.split(".")[-1]
+            if proj_name in packed_modules_mapping:
+                quant_type = None
+                shard_prefixes = [
+                    prefix.replace(proj_name, shard_proj_name) for shard_proj_name in packed_modules_mapping[proj_name]
+                ]
+                for shard_prefix in shard_prefixes:
+                    shard_quant_type = mock_quant_description[shard_prefix + ".weight"]
+
+                    if quant_type is None:
+                        quant_type = shard_quant_type
+                    elif shard_quant_type != quant_type:
+                        raise ValueError(
+                            f"Not all shards of {prefix} are quantized with same quant type."
+                            f"Shard {proj_name} uses {shard_quant_type}, but another shard"
+                            f"use {quant_type}. Please check quantization config."
+                        )
+            else:
+                quant_type = mock_quant_description[prefix + ".weight"]
         except KeyError as e:
             pytest.fail(f"KeyError was raised: {e}\n")
