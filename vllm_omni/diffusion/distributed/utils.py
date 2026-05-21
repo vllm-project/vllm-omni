68829f0cc1dbbf9a5d941e210a22c7c490c68019
Canlin Guo <961750412@qq.com>
2026-05-21 20:29:08 +0800

    Update vllm_omni/diffusion/distributed/utils.py
    Signed-off-by: Canlin Guo <961750412@qq.com>

--- vllm_omni/diffusion/distributed/utils.py
+++ vllm_omni/diffusion/distributed/utils.py
@@ -10,7 +10,7 @@ from vllm_omni.platforms import current_omni_platform
 
 def get_local_device() -> torch.device:
     """Return the torch device for the current rank based on detected device type."""
-    if torch.cuda.is_available() and torch.cuda.is_initialized():
-        return current_omni_platform.get_torch_device(torch.accelerator.current_device_index())
+    if current_omni_platform.is_initialized():
+        return current_omni_platform.get_torch_device(current_omni_platform.current_device())
     local_rank = int(os.environ.get("LOCAL_RANK", 0))
     return current_omni_platform.get_torch_device(local_rank)
