af61bdb79948643af10f9d93e3b2a6108a62548f
ZeldaHuang <hzm414167@alibaba-inc.com>
2026-05-27 11:23:03 +0800

    update stage config
    Signed-off-by: ZeldaHuang <hzm414167@alibaba-inc.com>

--- tests/helpers/stage_config.py
+++ tests/helpers/stage_config.py
@@ -325,18 +325,21 @@ _CI_OVERLAYS: dict[str, dict[str, Any]] = {
         "stages": [
             {
                 "stage_id": 0,
+                "max_num_seqs": 5,
                 "max_model_len": 32768,
                 "mm_processor_cache_gb": 0,
                 "default_sampling_params": {"max_tokens": 150, "ignore_eos": False},
             },
             {
                 "stage_id": 1,
+                "max_num_seqs": 5,
                 "gpu_memory_utilization": 0.5,
                 "max_model_len": 32768,
                 "default_sampling_params": {"max_tokens": 1000},
             },
             {
                 "stage_id": 2,
+                "max_num_seqs": 5,
                 "default_sampling_params": {"max_tokens": 2000},
             },
         ],
