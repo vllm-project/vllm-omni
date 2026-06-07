962c8aeae1983633ffd7f37b5b256f4e3f708650
xiaohajiayou <923390377@qq.com>
2026-06-07 17:45:00 +0800

    Refine stage deploy field grouping
    Signed-off-by: xiaohajiayou <923390377@qq.com>

--- tests/test_config_factory.py
+++ tests/test_config_factory.py
@@ -1619,6 +1619,8 @@ stages:
       step_execution: false
       vae_use_tiling: false
       enable_cpu_offload: false
+      max_generated_image_size: 1048576
+      tts_max_instructions_length: 1000
 """,
             encoding="utf-8",
         )
@@ -1657,6 +1659,8 @@ stages:
         assert stage.yaml_engine_args["step_execution"] is False
         assert stage.yaml_engine_args["vae_use_tiling"] is False
         assert stage.yaml_engine_args["enable_cpu_offload"] is False
+        assert stage.yaml_engine_args["max_generated_image_size"] == 1048576
+        assert stage.yaml_engine_args["tts_max_instructions_length"] == 1000
 
         stage.runtime_overrides = StageConfigFactory._merge_cli_overrides(
             stage,
@@ -1679,6 +1683,8 @@ stages:
                 "step_execution": True,
                 "vae_use_tiling": True,
                 "enable_cpu_offload": True,
+                "max_generated_image_size": 2097152,
+                "tts_max_instructions_length": 2000,
             },
         )
 
@@ -1690,6 +1696,8 @@ stages:
         assert omega_config.engine_args.step_execution is True
         assert omega_config.engine_args.vae_use_tiling is True
         assert omega_config.engine_args.enable_cpu_offload is True
+        assert omega_config.engine_args.max_generated_image_size == 2097152
+        assert omega_config.engine_args.tts_max_instructions_length == 2000
         assert omega_config.engine_args.parallel_config.pipeline_parallel_size == 2
         assert omega_config.engine_args.parallel_config.data_parallel_size == 3
         assert omega_config.engine_args.parallel_config.tensor_parallel_size == 4
