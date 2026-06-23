81c859c9e78166e1a291527da13e608c17ec974a
betta18 <jiangmengyu1@huawei.com>
2026-06-23 17:19:40 +0800

    fix ascend_config registry bug
    Signed-off-by: betta18 <jiangmengyu1@huawei.com>

--- vllm_omni/platforms/interface.py
+++ vllm_omni/platforms/interface.py
@@ -137,6 +137,11 @@ class OmniPlatform(Platform):
         return "vllm_omni.diffusion.worker.diffusion_model_runner.DiffusionModelRunner"
 
     @classmethod
+    def init_diffusion_worker_vllm_config(cls, vllm_config: Any) -> None:
+        """Initialize platform-specific state for diffusion worker VllmConfig."""
+        return None
+
+    @classmethod
     def get_torch_device(cls, local_rank: int | None = None) -> torch.device:
         raise NotImplementedError
 
