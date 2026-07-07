b8db798a85dc80d57ece19154cd76948133a7afb
yixiaoer <shin@yixiaoer.sg>
2026-07-07 09:21:58 +0800

    Add HiDream-O1-Image model
    Signed-off-by: yixiaoer <shin@yixiaoer.sg>

---
+++ vllm_omni/diffusion/models/hidream_o1_image/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+from vllm_omni.diffusion.models.hidream_o1_image.hidream_o1_image_transformer import (
+    HiDreamO1ImageTransformer,
+)
+
+__all__ = [
+    'HiDreamO1ImageTransformer',
+]
