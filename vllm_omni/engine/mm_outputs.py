2e3b3b9a50cf98a7e4acbffa0468dffbf83dc034
Boao Shi <aoibosh@connect.hku.hk>
2026-07-09 21:41:04 +0800

    re-export shims fix
    Signed-off-by: Boao Shi <aoibosh@connect.hku.hk>

---
+++ vllm_omni/engine/mm_outputs.py
@@ -0,0 +1,7 @@
+import warnings
+
+warnings.warn(
+    "Importing from 'vllm_omni.engine.mm_outputs' is deprecated. Use 'vllm_omni.outputs.mm_outputs' instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
---
+++ vllm_omni/engine/output_modality.py
@@ -0,0 +1,7 @@
+import warnings
+
+warnings.warn(
+    "Importing from 'vllm_omni.engine.output_modality' is deprecated. Use 'vllm_omni.outputs.output_modality' instead.",
+    DeprecationWarning,
+    stacklevel=2,
+)
