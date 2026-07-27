dea5df11db973614dc73e3b45df37ec27ee0e38e
weiyan <weiyanlin117@gmail.com>
2026-07-27 09:11:05 -0700

    add LongCat Avatar to diffusion alignment exclusions
    Signed-off-by: weiyan <weiyanlin117@gmail.com>

--- tests/model_tests/diffusion/test_alignment.py
+++ tests/model_tests/diffusion/test_alignment.py
@@ -43,6 +43,7 @@ EXCLUDED_MODELS = [
     "WanT2VDMD2Pipeline",
     "WanI2VDMD2Pipeline",
     "LongCatImagePipeline",
+    "LongCatVideoAvatarPipeline",
     "BagelPipeline",
     "BooguImagePipeline",
     "LancePipeline",
