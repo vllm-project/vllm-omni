8b6a6066b9aadb18468d9ac6868214ca1fb27bd3
Yifan Zong <yzong@redhat.com>
2026-08-13 20:09:55 -0400

    Fix
    Signed-off-by: Yifan Zong <yzong@redhat.com>

--- vllm_omni/diffusion/models/longcat_image/pipeline_longcat_image.py
+++ vllm_omni/diffusion/models/longcat_image/pipeline_longcat_image.py
@@ -627,6 +627,7 @@ class LongCatImagePipeline(nn.Module, CFGParallelMixin, DiffusionPipelineProfile
                 negative_kwargs = {
                     "hidden_states": latents,
                     "timestep": timestep / 1000,
+                    "guidance": guidance,
                     "encoder_hidden_states": negative_prompt_embeds,
                     "txt_ids": negative_text_ids,
                     "img_ids": latent_image_ids,
