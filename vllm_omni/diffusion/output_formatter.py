1b25583afcaafa26c589c09826c730b1492aa95f
yuanwu <yuan.wu@intel.com>
2026-08-17 06:08:09 -0700

    [Diffusion] Add config-driven Wan EG/EGD topology
    Signed-off-by: yuanwu <yuan.wu@intel.com>

--- vllm_omni/diffusion/output_formatter.py
+++ vllm_omni/diffusion/output_formatter.py
@@ -117,6 +117,7 @@ def format_empty_diffusion_outputs(
             latents=None,
             custom_output=custom_output,
             finished=finished,
+            custom_output=custom_output or {},
         )
     ]
 
