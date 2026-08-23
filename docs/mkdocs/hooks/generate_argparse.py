6f4baa9e900fa03ab80b89b103b1a7b2316aa0b4
MosCloud <bwzhang1991@163.com>
2026-08-23 01:34:54 +0000

    [Doc] Fix video response worker CLI generation
    Signed-off-by: MosCloud <bwzhang1991@163.com>

--- docs/mkdocs/hooks/generate_argparse.py
+++ docs/mkdocs/hooks/generate_argparse.py
@@ -124,6 +124,8 @@ def extract_omni_serve_subparser_init():
                         "make_arg_parser": lambda parser: parser,  # no-op for doc
                         "_ensure_vllm_platform": lambda: None,  # no-op for doc
                         "_nonneg_finite_float": float,
+                        "_video_response_frame_conversion_workers": int,
+                        "MAX_FRAME_CONVERSION_WORKERS": 8,
                         "VLLM_SUBCMD_PARSER_EPILOG": "",
                         "logger": logger,
                         "DummySubparsers": DummySubparsers,
