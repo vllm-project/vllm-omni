17cc90008e6a75b06b3f6ca042504aef74aa9077
wy17003 <yaow17003@gmail.com>
2026-08-23 11:08:01 +0800

    rename omni_connector_support
    Signed-off-by: wy17003 <yaow17003@gmail.com>

--- vllm_omni/engine/arg_utils.py
+++ vllm_omni/engine/arg_utils.py
@@ -12,7 +12,7 @@ from vllm_omni.config import OmniModelConfig
 from vllm_omni.outputs.output_modality import OutputModality
 from vllm_omni.platforms import current_omni_platform
 from vllm_omni.plugins import load_omni_general_plugins
-from vllm_omni.worker.omni_connector_support import validate_worker_omni_connector
+from vllm_omni.worker.omni_connector_validation import validate_worker_omni_connector
 
 logger = init_logger(__name__)
 
