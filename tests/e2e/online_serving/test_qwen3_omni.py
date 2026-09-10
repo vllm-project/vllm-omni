72d96b55dae431305280922b536b2ec3f1cce3eb
wangyu <410167048@qq.com>
2026-09-10 18:19:55 +0800

    remove debug
    Signed-off-by: wangyu <410167048@qq.com>

--- tests/e2e/online_serving/test_qwen3_omni.py
+++ tests/e2e/online_serving/test_qwen3_omni.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM-Omni project
+
 """
 E2E Online tests for Qwen3-Omni model with video input and audio output.
 """
